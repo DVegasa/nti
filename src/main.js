8d227c98f11c4c1a11efaf83bd66568b1261dda5
UDK <udk1@bk.ru>
2022-07-15 03:13:34 +0300

    Add component chat admin

--- src/main.js
+++ src/main.js
@@ -5,9 +5,11 @@ import router from './router'
 import store from './store'
 import 'element-plus/dist/index.css'
 import ElementPlus from 'element-plus'
+import Chat from 'vue3-beautiful-chat'
 
 createApp(App)
     .use(ElementPlus)
     .use(store)
     .use(router)
+    .use(Chat)
     .mount('#app')
