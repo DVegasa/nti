ff05003449160d83bc80a18e410907e6e5d0e081
UDK <udk1@bk.ru>
2022-07-15 03:13:34 +0300

    Add component chat admin

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,36 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    messages: []
   },
   getters: {
+    getAllMessages(state) {
+      return state.messages;
+    }
   },
   mutations: {
+    getMessages(state, messages) {
+      state.messages = messages;
+    },
+    addMessages(state, message) {
+      state.messages.push(message);
+    }
   },
   actions: {
+    async getMessages(context) {
+      const messages = await fetch("http://localhost:8080/chat");
+      context.commit('getMessages', await messages.json());
+    },
+    async sendMessage(context, params) {
+      const message = await fetch("http://localhost:8080/chat", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(params)
+      })
+      context.commit('addMessages', params);
+    }
   },
   modules: {
   }
